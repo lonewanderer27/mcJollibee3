3925fa6c097062ff19bba7d99fee6cc7c15d56a7
lonewanderer27 <adrianejames27@gmail.com>
2021-12-14 02:42:41 +0800

    working add order and fixed lots of bugs

--- Program.cs
+++ Program.cs
@@ -64,6 +64,43 @@ namespace mcJollibee3
                                                                                                                    |                                         |   
                                                                                                                    +-----------------------------------------+
         ";
+
+        static String paymentScreen = @"
+     __  __                                                                                                             ___            _                  
+    |  \/  |  ___  _ __   _   _                                                                                        / _ \  _ __  __| |  ___  _ __  ___ 
+    | |\/| | / _ \| '_ \ | | | |                                                                                      | | | || '__|/ _` | / _ \| '__|/ __|
+    | |  | ||  __/| | | || |_| |                                                                                      | |_| || |  | (_| ||  __/| |   \__ \
+    |_|  |_| \___||_| |_| \__,_|                                                                                       \___/ |_|   \__,_| \___||_|   |___/
+
+
+     ___________________________        ___________________________        ___________________________             +-----------------------------------------+
+    |                           |      |                           |      |                           |            |                                         |
+    |     Chicken with Rice     |      |           Burger          |      |           Fries           |            |                McJollibee               |
+    |                           |      |                           |      |                           |            |                                         |  
+    |    ₱100    Code: cwr      |      |    ₱50    Code: bgr       |      |    ₱45    Code: frs       |            |                                         |
+    |___________________________|      |___________________________|      |___________________________|            |         Edsa Corner Evangelista         |
+                                                                                                                   |             Bangkal, Makati             |
+                                                                                                                   |               Metro Manila              |
+     ___________________________        ___________________________                                                |                                         |
+    |                           |      |                           |                                               |                                         |
+    |           Sundae          |      |         Softdrinks        |                                               |     Qty      Item            Price      |
+    |                           |      |                           |                                               |                                         |
+    |    ₱30    Code: sde       |      |    ₱20    Code: sds       |                                               |                           ₱             |
+    |___________________________|      |___________________________|                                               |                           ₱             |
+                                                                                                                   |                           ₱             |
+                                                                                                                   |                           ₱             |
+                                                                                                                   |                           ₱             |
+                                                                                                                   |  _____________________________________  |
+                                                                                                                   |                                         |
+                                                                                                                   |              Subtotal     ₱             |
+                                                                                                                   |                                         |
+                                                                                                                   |             + 10% VAT     ₱             |
+                                                                                                                   |  _____________________________________  |
+                                                                                                                   |                                         |
+                                                                                                                   |        Total with VAT:    ₱             |
+                                                                                                                   |                                         |   
+                                                                                                                   +-----------------------------------------+
+        ";
         
         static String addMenu = @"
     To add an item, enter its code shown above                          
@@ -79,9 +116,10 @@ namespace mcJollibee3
         */
 
         static Double[] itemPrices =  {100.00, 50.00, 45.00, 30.00, 20.00};
+        static String[] itemCodes = {"cwr", "bgr", "frs", "sde", "sds"};
         static Double[] totalItemPrices = new Double[5];
         static int[] orderQuantities = new int[5];
-        static int totalQty;
+        static int totalQty, num_userChoice;
         static Double vat, subtotalPrice, totalPrice;
 
         static string currentUser = "", userChoice = "";
@@ -219,15 +257,16 @@ namespace mcJollibee3
         }
 
 
-        static String getcurrentUser(String currentUser) {
+        static String getcurrentUser() {
             int run = 1;
+            String currentUser = "";
             while (currentUser == "") {
                 Console.SetCursorPosition(53,30);
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("What is your name? ");
                 currentUser = Console.ReadLine();
                 if (run != 0) {
-                    error(53, 32);
+                    error(53, 32, "You typed invalid name, please try again");
                 }
                 run++;
             }
@@ -236,54 +275,185 @@ namespace mcJollibee3
 
 
 
-        static void error(int col, int row) {
+        static void error(int col, int row, string errorMessage = "Error. Please try again") {
+            /*
+            This module provides error message to the user.
+            It takes the values of column and row to determine where it will display the error.
+            It can also take a custom message via errorMessage parameter, otherwise it displays the default message.
+            */
+            Console.SetCursorPosition(col, row);
+            Console.Write("                                                                           ");   //clears whatever the user typed in before
             Console.SetCursorPosition(col, row);
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("Error. Please try again");
+            Console.Write(errorMessage);
             Console.ForegroundColor = ConsoleColor.White;
         }
 
 
 
-        
+        static void success(int col, int row, string successMessage = "Action success, proceeding...") {
+            /*
+            This module provides success message to the user.
+            It takes the values of column and row to determine where it will display the success message.
+            It can also take a custom message via successMessage parameter, otherwise it displays the default message.
+            */
+            Console.SetCursorPosition(col, row);
+            Console.Write("                                                                           ");   //clears whatever the user typed in before
+            Console.SetCursorPosition(col, row);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(successMessage);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+
+        static String typeChoice(String errorMessage="Error. Please try again.", bool errorEnable = true) {
+            int run  = 1;            
+            userChoice = " ";    //not overriding this causes it to take the previous 
+            while (userChoice == " ") {
+                Console.SetCursorPosition(5,30);    //it feels weird that C# doesn't support multiplying strings using integers, what a shame tbh...
+                Console.Write("                                                                           ");   //clears whatever the user typed in before
+                Console.SetCursorPosition(5,30);
+                Console.Write("Type your choice: ");
+                userChoice = Console.ReadLine();
+                if (run != 1) {
+                        if (errorEnable == true) {
+                            error(5, 32, errorMessage);
+                        }
+                    }
+                    run++;
+            }
+            //Debugging purposes...
+            //Console.Write("typeChoice succeeded?");
+            //Console.ReadLine();
+            return userChoice;
+        }
+
+
+
+        static int typeNum() {
+            int verified_typeNum_input;
+            String userNum;
+            bool success_typeNum;
+
+            do {
+                Console.SetCursorPosition(5,30);
+                Console.Write("                                                                           ");   //clears whatever the user typed in before
+                Console.SetCursorPosition(5,30);
+                Console.Write("Type your choice: ");
+                userNum = Console.ReadLine();
+                success_typeNum = Int32.TryParse(userNum, out verified_typeNum_input);
+                if (success_typeNum) {
+                    return verified_typeNum_input;
+                }
+                else {
+                    error(5, 32, errorMessage: "Invalid number, please try again.");
+                }
+            }
+            while (success_typeNum == false);
+
+            return verified_typeNum_input;
+        }
+
+
+
+        static string[] splitString(String str) {
+            String[] spearator = { ", " };
+            String[] strlist = str.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+            return strlist;
+        }
+
+
+
+        static string determineItemName(String itemCode) {
+            string itemName = "";
+
+            if (itemCode == "cwr") {
+                itemName = "Chicken with Rice";
+            }
+
+            if (itemCode == "bgr") {
+                itemName = "Burger";
+            }
+
+            if (itemCode == "frs") {
+                itemName = "Fries";
+            }
+
+            if (itemCode == "sde") {
+                itemName = "Sundae";
+            }
+
+            if (itemCode == "sds") {
+                itemName = "Softdrinks";
+            }
+
+            return itemName;
+        }
+
+
+
         static void welcomeScreen() {
             Console.Clear();
             //column, row
             Console.SetCursorPosition(15,10);
             Console.Write(banner);
 
-            Console.SetCursorPosition(53,27);
-            Console.Write("For a better experience, please use a fixed width font ");
+            Console.SetCursorPosition(54,27);
+            Console.Write("For better experience, please use a fixed width font ");
             Console.ReadLine();
 
             if (program_execution_count == 0) {
                 
-                currentUser = getcurrentUser(Console.ReadLine());
+                currentUser = getcurrentUser();
             }
             
 
             
         }
-
 
 
 
         static void addOrder() {
-            Console.Write("addOrder got triggered");
-            int run  = 1;
-            while (userChoice == "") {
+            int counter;
+            bool success_addOrder = false, done_addOrder = false;
+            userChoice = "";
+            while (done_addOrder == false) {
                 Console.SetCursorPosition(5, 25);
-                Console.Write(addMenu);
-                Console.SetCursorPosition(5, 30);
-                Console.Write("Type your choice: ");
-                userChoice = Console.ReadLine();
-                if (run != 0) {
-                    error(5, 32);
+                Console.Write("To add items, enter its code and amount. Example: frs, 2                              ");
+                Console.SetCursorPosition(5, 26);
+                Console.Write("Type 'e' once finished.");
+
+                userChoice = typeChoice(errorEnable: false);
+                String[] array_userChoice = splitString(userChoice);
+
+                if (userChoice == "e") {
+                    success(5, 32, "");
+                    done_addOrder = true;
+                    break;
                 }
-                run++;
+
+                for (counter=0; counter < 5; counter++) {
+                    if (array_userChoice[0] == itemCodes[counter])
+                    {    
+                        totalItemPrices[counter] += (itemPrices[counter] * Int32.Parse(array_userChoice[1]));
+                        success_addOrder = true;
+                        done_addOrder = true;
+                        success(5, 32, $"Successfully added ");
+                        break;
+                    }
+                }
+
+                if (userChoice == "") {
+                    error(5, 33);
+                }
+
+                else if (success_addOrder == false) {
+                    error(5,33, $"Item code '{array_userChoice[0]}' is not found, please try again.");
+                }
             }
-            
         }
+            
 
         static void cancelOrder() {
             
@@ -297,25 +467,6 @@ namespace mcJollibee3
         
         static void clearOrder() {
             
-        }
-
-
-
-        static String choicesMenu() {
-            int run=1;
-            while (userChoice == "") {
-                userChoice = userChoice.ToLower();
-                Console.SetCursorPosition(5, 25);
-                Console.Write("OPTIONS:     [1]Add      [2]Cancel Orders    [3]Payment      [4]Exit");
-                Console.SetCursorPosition(5, 30);
-                Console.Write("Type your choice: ");
-                userChoice = Console.ReadLine();
-                if (run != 0) {
-                    error(5, 32);
-                }
-                run++;
-            }
-            return userChoice;
         }
 
         
@@ -332,21 +483,23 @@ namespace mcJollibee3
                 //Overlays the values of the orders on top of the main screen's template.
                 updateOrders();
 
-                userChoice = choicesMenu();
-                if (userChoice == "1" || userChoice == "add") {
+                Console.SetCursorPosition(5, 25);
+                Console.Write("OPTIONS:     [1]Add      [2]Cancel Orders    [3]Payment      [4]Exit");
+                num_userChoice = typeNum();
+                if (num_userChoice == 1) {
                     addOrder();
                 }
-                else if (userChoice == "2" || userChoice == "cancel") {
+                else if (num_userChoice == 2) {
                     cancelOrder();
                 }
-                else if (userChoice == "3" || userChoice == "payment") {
+                else if (num_userChoice == 3) {
                     payment();
                 }
-                else if (userChoice == "4" || userChoice == "exit") {
-
+                else if (num_userChoice == 4) {
+                    
                 }
                 else {
-                    
+                    error(5, 32, "Invalid number choice, please try again.");
                 }
 
             
